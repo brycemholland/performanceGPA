58fb3c66c582375da3efe15865c3bc14cc835418
Bryce Holland <bholland@performancegpa.com>
2016-04-07 15:09:03 -0500

    do work

--- javascripts/footer.js
+++ javascripts/footer.js
@@ -5,17 +5,12 @@ $(document).ready(function(){
   var $footer = $(".footer");
   var $main = $(".main-container");
   var $navbar = $(".navbar");
-  var $header = $(".header");
-  var $menu = $(".menu");
-  var $shadow = $(".shadow");
 
 
 // Setup =================================================
   
   function adjustFooter(){
-    if ($(window).width() >= 960){
-      $main.css("min-height", ($main.height()-$header.height()-$navbar.height()-$footer.height()-56)+"px" );
-    } else if (($(window).width() < 960) && ($(window).width() > 600)){
+    if ($(window).width() > 600){
       $main.css("min-height", ($main.height()-$navbar.height()-$footer.height()-56)+"px" );
     } else {
       $main.css("min-height", ($main.height()-$navbar.height()-$footer.height()-40)+"px" );
@@ -29,15 +24,5 @@ $(document).ready(function(){
   $( window ).on( "orientationchange", function() {
     adjustFooter();
   });
-
-  /*
-  console.log($('body').height());
-  if ($footer.offset().top < $(window).height()-$footer.height()){
-    $footer.css({
-      'position':'absolute',
-      'bottom':'0'
-    })
-  }
-  */
 
 });
